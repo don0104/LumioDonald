aaad57dd3391777c107417a0d5eedeb55f35d70e
don0104 <donaldlumio1@gmail.com>
2025-10-13 14:09:29 +0800

    3rd commit

--- app/config/config.php
+++ app/config/config.php
@@ -79,7 +79,7 @@ $config['ENVIRONMENT']             = 'development';
 | WARNING: You MUST set this value!
 |
 */
-$config['base_url'] 				= 'https://ricablanca-aienpogi.onrender.com';
+$config['base_url'] 				= 'https://lumiodonald-1.onrender.com/';
 
 /*
 |--------------------------------------------------------------------------
